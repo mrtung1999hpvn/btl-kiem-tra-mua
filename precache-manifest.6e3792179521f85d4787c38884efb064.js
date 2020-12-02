3df47a01cd16159b0c01933ec254201297dae67a
mrtung1999hpvn <tung75397@st.vimaru.edu.vn>
2020-12-02 22:10:08 +0700

    Updates

---
+++ precache-manifest.6e3792179521f85d4787c38884efb064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a74a7ad4b7fa7ec41eea2dd5e20977",
+    "url": "/btl-kiem-tra-mua/index.html"
+  },
+  {
+    "revision": "49675305a600ea3290bc",
+    "url": "/btl-kiem-tra-mua/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "0f774a018c66c5402553",
+    "url": "/btl-kiem-tra-mua/static/js/2.ca0a8b10.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/btl-kiem-tra-mua/static/js/2.ca0a8b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49675305a600ea3290bc",
+    "url": "/btl-kiem-tra-mua/static/js/main.c28d5665.chunk.js"
+  },
+  {
+    "revision": "fbc4e1d2f495c034552e",
+    "url": "/btl-kiem-tra-mua/static/js/runtime-main.4df2c60b.js"
+  }
+]);
